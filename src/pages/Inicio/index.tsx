8715f6ae73b28429ef1dd593928645e96f80a40e
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-06-15 17:43:05 -0300

    Page-home && Eslint

---
+++ src/pages/Inicio/index.tsx
@@ -0,0 +1,14 @@
+import cardapior from 'pages/Cardapio/itens/itens.json';
+
+export default function Inicio() {
+  return (
+    <section>
+      <h3>
+        Recomendações da Cozinha
+      </h3>
+      <div>
+
+      </div>
+    </section>
+  );
+}
